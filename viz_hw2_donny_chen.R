56b592c0e4029d5e407d4ba70391cfb530ad26ff
Donny Chen <dchen25@dons.usfca.edu>
2017-04-17 16:35:54 -0700

    minor fix

--- viz_hw2_donny_chen.R
+++ viz_hw2_donny_chen.R
@@ -1,5 +1,10 @@
+# This shiny app satisfies all the basic requirments for the hw assignment.
+# Obviously, there are more features can be added/optimized for this plot, 
+# but due to time limitation and other life/work priorities they are not included as of now.
+
 rm(list = ls())
-cat("\014") 
+cat("\014")
+
 ########################################################################
 # load required libraries (install if libraries not found)
 if ("shiny" %in% rownames(installed.packages())==F) {
@@ -50,12 +55,12 @@ unique_region <- unique(c(as.character(df_meta_p$Region)))[-5]
 # Shiny UI
 ui <- fluidPage(
   headerPanel('Life Expectancy and Fertility Rate'),
-  fluidRow(
-    mainPanel(uiOutput("ggvis_ui"), ggvisOutput("ggvis"))),
-  fluidRow(column(12,
-    checkboxGroupInput("region", "Select Regions", choices = unique_region, inline = TRUE)), offset=2),
-  fluidRow(column(12,sliderInput("year", "Select Year", 1960, 2014, 1, animate = TRUE)), offset=2)
-)
+  fluidRow(mainPanel(uiOutput("ggvis_ui"), ggvisOutput("ggvis"))),
+  fluidRow(column(12,sliderInput("year", "Select Year", 1960, 2014, 1, animate = TRUE, sep="")), offset=2),
+  fluidRow(column(12,checkboxGroupInput("region", "Select Regions", choices = unique_region, inline = TRUE)), offset=2),
+  fluidRow(column(12,sliderInput("size", "Select Size", 1, 10, 1, value = 5)), offset=2)
+  )
+
 # Shiny Server
 server <- function(input, output) {
   
@@ -63,49 +68,57 @@ server <- function(input, output) {
     if(is.null(x)) return(NULL)
     paste0 ( x$Country.Name )
   }
-    
-  # region_select <- reactive(is.null(input$region))
-    
+
+  normalize <- function (x) {
+    normalized = (x-min(x, na.rm=TRUE))/(max(x, na.rm=TRUE)-min(x, na.rm=TRUE))
+    normalized}
+  
   viz <- reactive({
     if (is.null(input$region)){
     Fertility.Rate <- df_meta_fr[, as.character(input$year)]
     Life.Expectancy <- df_meta_le[, as.character(input$year)]
     Population <- df_meta_p[, as.character(input$year)]
+    Population <- normalize(Population)*input$size*300
     data <- data.frame(cbind(Fertility.Rate, Life.Expectancy, Population))
     data$Country.Name <- df_meta_fr$Country.Name
     data$region_select <- 0
     data[df_meta_fr[,"Region"]%in%input$region, "region_select"] <- 1
+    
     data %>% 
-      ggvis(~Life.Expectancy, ~Fertility.Rate, size = ~Population, opacity := 1,
+      ggvis(~Life.Expectancy, ~Fertility.Rate, size := ~Population, opacity := 1,
             fill = ~region,
             key := ~Country.Name) %>%
       layer_points() %>%
       hide_legend("size") %>%
+      scale_numeric("x", domain = c(20, 85), clamp = TRUE) %>%
+      scale_numeric("y", domain = c(0, 9), clamp = TRUE) %>%
       add_tooltip(show_country, "hover")
-    
     }
     else{
     # input <- data.frame(region="North America", year=2014)
       Fertility.Rate <- df_meta_fr[, as.character(input$year)]
       Life.Expectancy <- df_meta_le[, as.character(input$year)]
       Population <- df_meta_p[, as.character(input$year)]
+      Population <- normalize(Population)*input$size*300
       data <- data.frame(cbind(Fertility.Rate, Life.Expectancy, Population))
       data$Country.Name <- df_meta_fr$Country.Name
       data$region_select <- 0.2
       data[df_meta_fr[,"Region"]%in%input$region, "region_select"] <- 1
-    data %>% 
-      ggvis(~Life.Expectancy, ~Fertility.Rate, size = ~Population, opacity :=~region_select,
-            fill = ~region,
-            key := ~Country.Name) %>%
-      layer_points() %>%
-      hide_legend("size") %>%
-      add_tooltip(show_country, "hover")
+      data %>% 
+        ggvis(~Life.Expectancy, ~Fertility.Rate, size := ~Population, opacity :=~region_select,
+              fill = ~region,
+              key := ~Country.Name) %>%
+        layer_points() %>%
+        hide_legend("size") %>%
+        scale_numeric("x", domain = c(20, 85), clamp = TRUE) %>%
+        scale_numeric("y", domain = c(0, 9), clamp = TRUE) %>%
+        add_tooltip(show_country, "hover")
     }}
     )
-
-      viz%>%bind_shiny("ggvis", "ggvis_ui")
+    
+  viz%>%bind_shiny("ggvis", "ggvis_ui")
 }
 
 shinyApp(ui = ui, server = server)
 
-# shiny::runGitHub(repo = "usfviz/donnydongchen-hw2", username = "donnydongchen", subdir = "viz_hw2_donny_chen.R")
+# shiny::runGitHub(repo = "usfviz/donnydongchen-hw2", username = "usfviz", subdir = "viz_hw2_donny_chen.R")
